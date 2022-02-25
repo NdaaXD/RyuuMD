7e1500cbb26d3ffda16ebf109404c5d0a533fbb3
NdaaXD <ndaatzy123@gmail.com>
2022-02-26 04:16:26 +0700

    MD

---
+++ ryudata/sewa.js
@@ -0,0 +1,24 @@
+exports.sewa = () => {
+	return`🇸 🇪 🇼 🇦  🇧 🇴 🇹 ❗
+
+┏━━━━━━
+┃𝗛𝗔𝗥𝗚𝗔 𝗦𝗘𝗪𝗔 :
+┃ *1 MINGGU : Rp 5.000*
+┃ *1 BULAN : Rp 15.000*
+┃ *PERMANEN : Rp 35.000*
+┗━━━━━━
+┏━━━━━━
+┃𝗙𝗜𝗧𝗨𝗥 𝗕𝗢𝗧 :
+┃ *⚔️ Antilink Group*
+┃ *👨‍🎨 Buat Stiker*
+┃ *📥 Downloader*
+┃ *Dll.*
+┗━━━━━━
+Note:Ini Bot Gratis Masuk Group Ya Jadi Ga Usah Sewa!!.
+
+*SISTEM: TES BOT-MASUKIN BOT-TF-DONE*
+
+*MINAT? CHAT :
+wa.me/6288220118685
+`
+}
